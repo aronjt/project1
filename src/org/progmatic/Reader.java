df205a0251cd2aae1951c8f246554f83d36fd403
aronjt <jtelekiaron@gmail.com>
2020-11-11 11:25:51 +0100

    perfect reader

---
+++ src/org/progmatic/Reader.java
@@ -0,0 +1,34 @@
+package org.progmatic;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.spi.AbstractResourceBundleProvider;
+
+public class Reader {
+
+    private static ArrayList<String> text = new ArrayList<>();
+
+    public Reader() {
+    }
+
+    public static ArrayList<String> read() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("files/proba.txt"));
+        while (sc.hasNext()) {
+            String word = sc.next();
+            char[] wordArray = word.toCharArray();
+            if (!Character.isLetter(wordArray[wordArray.length-1])) {
+                StringBuilder newWord = new StringBuilder();
+                for (int i = 0; i < wordArray.length - 1; i++) {
+                    newWord.append(wordArray[i]);
+                }
+            }else {
+                    text.add(word);
+                }
+        }
+
+        return text;
+    }
+}
--- src/org/progmatic/Szemantika.java
+++ src/org/progmatic/Szemantika.java
@@ -1,4 +1,17 @@
 package org.progmatic;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Szemantika {
+
+    private static ArrayList<String> text = new ArrayList<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        text = Reader.read();
+
+        System.out.println(text);
+    }
+
 }
