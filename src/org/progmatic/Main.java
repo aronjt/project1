6c37f3d85f7b3a5231b6322a2f07377f6cd764c5
NOBIPROG <ispannorbert@gmail.com>
2020-11-11 13:30:31 +0100

    Bullshit first method+main test

--- src/org/progmatic/Main.java
+++ src/org/progmatic/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         Reader read = new Reader();
+        Bullshit bullshit= new Bullshit(read.read("files/proba.txt"));
         Szemantika szem = new Szemantika(read.read("files/proba.txt"));
         System.out.println(szem);
+        System.out.println("BullShit: ");
+        bullshit.generateBullShitRandomOrderFromText(read.read("files/proba.txt"),10);
     }
 }
